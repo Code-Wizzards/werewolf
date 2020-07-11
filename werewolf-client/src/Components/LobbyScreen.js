dab457a8566d71906ab6f246613b3f758a3a23da
Chris Dromey <chrisdromey@gmail.com>
2020-07-11 18:41:28 +0100

    styled the lobby screen some more

--- werewolf-client/src/Components/LobbyScreen.js
+++ werewolf-client/src/Components/LobbyScreen.js
@@ -1,8 +1,20 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, Keyboard} from 'react';
 import { GameContext } from '../gameManager/game-manager'
+import { makeStyles } from '@material-ui/core/styles';
 import PlayerList from './PlayerList'
-import { Container, Box, Button, TextField } from '@material-ui/core';
+import { Container, Box, Button, TextField, Paper, LinearProgress, Typography } from '@material-ui/core';
 import { setPlayerStatus } from '../RestServer'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    padding: theme.spacing(1),
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  }
+}))
 
 const LobbyScreen = ({ players }) => {
   const [ statusInput, setStatusInput ] = useState('')
@@ -12,28 +24,43 @@ const LobbyScreen = ({ players }) => {
     setStatusInput(e.target.value)
   }
 
-  const handleClick = () => {
-    setPlayerStatus(userId, gameId, statusInput)
-    setStatusInput('')
+  const handleKeyPress = (e) => {
+    if(e.keyCode == 13){
+      setPlayerStatus(userId, gameId, statusInput)
+      setStatusInput('')
+      document.activeElement.blur();
+    }
   }
+
+
+  const classes = useStyles();
 
   return (
     <Container>
 
-      <PlayerList players={players} />
-      <Button color="primary" variant="contained" align="center" >
-        Start The Game
-      </Button>
-
-      <Box>
-        <Box p={3}>
-          <TextField value={statusInput} id="status-input" label="Set a Status" variant="outlined" onChange={handleChange}/>
-          <Button color="secondary" variant="contained" align="center" onClick={handleClick}>
-            Set
-        </Button>
-        </Box>
+      <Box p={1}>
+        <Paper elevation={3}>
+          <PlayerList players={players} />
+        </Paper>
       </Box>
 
+      <Box p={1} textAlign='center'>
+      <Typography
+                component="span"
+                variant="body1"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                Waiting for more players to join
+              </Typography>
+        <LinearProgress />
+      </Box>
+      <div className={classes.root}>
+        <TextField value={statusInput} id="status-input" label="Set a Status" variant="outlined" onChange={handleChange} onKeyDown={handleKeyPress}/>
+        <Button color="primary" variant="contained" >
+          Start The Game
+        </Button>
+      </div>
     </Container>
   )
 }
